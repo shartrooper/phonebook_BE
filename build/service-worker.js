1f68a138b8acce471c5c977f687a9657b65fc902
Marko <markoantonio.amonamarth@gmail.com>
2019-09-19 12:43:00 -0400

    added validations and error handling

--- build/service-worker.js
+++ build/service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.897a7b46e5ecd9b6b9e42f69a6133e57.js"
+  "/precache-manifest.a82a22a909250ba1e58df7d62463821f.js"
 );
 
 self.addEventListener('message', (event) => {
